e69da203e10d75d6d16547be8725fc4a99293ced
Chris Thompson <teaforthecat@gmail.com>
2018-11-11 14:46:53 +0000

    feature complete, plus layer 3

---
+++ keyboards/minidox/keymaps/teaforthecat/config.h
@@ -0,0 +1,7 @@
+#include "config_common.h"
+#define TAPPING_TERM 200
+#define FORCE_NKRO
+#define QMK_KEYS_PER_SCAN 4
+#define RETRO_TAPPING
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
